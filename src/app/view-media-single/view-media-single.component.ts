4ffccb134932b6ecab35b0813e82408275a44660
saphiros <mickael.lemieux@laposte.net>
2020-06-21 22:26:55 +0200

    Finission , ajout films

--- src/app/view-media-single/view-media-single.component.ts
+++ src/app/view-media-single/view-media-single.component.ts
@@ -33,6 +33,70 @@ export class ViewMediaSingleComponent implements OnInit,OnDestroy {
       resume: 'Un flic pro du kung fu doit arreter le Kung Fürher',
       image: 'Kung Fury.png'
     },
+    {
+      id: 3,
+      titre: 'Le Parrain',
+      type: 'film',
+      genre: 'Policier/Drame',
+      resume: 'Un parrain qui tente de tuer Don Vito',
+      image: 'LeParrain.jpg'
+    },
+    {
+      id: 4,
+      titre: 'Les évadés',
+      type: 'film',
+      genre: 'Drame',
+      resume: "Deux gars qui veulent s'évader",
+      image: 'Evadee.jpg'
+    },
+    {
+      id: 5,
+      titre: 'YourName',
+      type: 'film',
+      genre: 'Animation/Fantastique',
+      resume: "Deux jeunes ado qui échangent leurs corps",
+      image: 'YourName.jpg'
+    },
+    {
+      id: 6,
+      titre: 'The Dark Knight',
+      type: 'film',
+      genre: 'Action/Thriller',
+      resume: "Batman contre Joker dans Gotham",
+      image: 'Batman.jpg'
+    },
+    {
+      id: 7,
+      titre: 'Pulp Fiction',
+      type: 'film',
+      genre: 'Policier/Thriller',
+      resume: "Il y a quelques malformation génétiques chez certaines demoiselles",
+      image: 'PulpFiction.jpg'
+    },
+    {
+      id: 8,
+      titre: 'La Ligne Verte',
+      type: 'film',
+      genre: 'Policier/Fantastique',
+      resume: "John Cafe qui nous fait un bon café ( et de bon miracles )",
+      image: 'LigneVerte.jpg'
+    },
+    {
+      id: 9,
+      titre: 'Forest Gump',
+      type: 'film',
+      genre: 'Romance/Drame/Comédie',
+      resume: "Un homme qui passa sa vie à courrir et qui créea le Smiley",
+      image: 'Forest.jpg'
+    },
+    {
+      id: 10,
+      titre: 'Le roi Lion',
+      type: 'film',
+      genre: 'Animation',
+      resume: "Le film comprennant mufassa, traumatisant une partie des milléniales dès leurs plus jeune âge",
+      image: 'Lion.jpg'
+    },
   ]
   
   id: number;
